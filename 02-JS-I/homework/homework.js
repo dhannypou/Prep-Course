a666a906ce05f57b9ef8126c504f2af50716198d
Daniela Pourrieux <dhannychocolateteamo@gmail.com>
2022-07-29 11:40:02 -0300

    tarea 1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -140,7 +140,7 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  return num * num;
+  return num ** 2;
   
 }
 
---
+++ 02-JS-I/homework/tarea.js
@@ -0,0 +1,8 @@
+///* una variable es un lugar donde guardo informacion de un dato y se puede cambiar mientras este desarrollando el codigo*/
+/* un string es un tipo de datos, una cadena de caracteres que se reprensentan dentro de comillas, pueden ser simples o dobles*/
+/* un funcion es un bloke de codigo que puedo reutilizar, es decir sirven para agrupar codigos que voy a usar varias veces
+la sentencia return sirve para darle fin a un codigo y tambien sirve para devolver un resultado.
+un argumento puede ser una variable o una funcion externa al bloque de codigo
+son valores que le paso a mi funcion para poder realizar algo dentro del codigo*/
+/*el bucle if sirve para comparar dentro de un bloque de codigo y devuelve un resultado */
+/*un booleano es un tipo de dato que puede ser true o false*/
